1afacb6afdf222f35ee32619d52ef84bdbd0c5a2
Gabriel Vicent <rodriguezisaac876@gmail.com>
2025-01-17 23:05:19 -0400

    Boton login
    En funcion. Listo para su uso

--- main.py
+++ main.py
@@ -15,15 +15,11 @@ class Widget(QWidget):
         super().__init__(parent)
         self.ui_login = Ui_Form()
         self.ui_login.setupUi(self)
-        #self.ui_login.pushButton.clicked.connect(self.cerrar_ventana)
-        #self.ui_login.pushButton_2.clicked.connect(self.close)
+        self.ui_login.pushButton.clicked.connect(self.cerrar_ventana)
 
     def cerrar_ventana(self):
         self.close()
         self.facturacion()
-        
-    def cerrar_ventana_login(self):
-        self.close()
 
     def facturacion(self):
         self.ventana_facturacion = QtWidgets.QMainWindow()
