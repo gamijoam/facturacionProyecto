be32faf2b09b67aca1f11b7c2a2dbc0eac688473
Gabriel Vicent <rodriguezisaac876@gmail.com>
2025-01-19 20:36:22 -0400

    Rediseño de la GUI facturacion

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # This Python file uses the following encoding: utf-8
 import sys
 from PySide6 import QtWidgets
-from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtWidgets import QApplication, QWidget, QTableWidgetItem
 
 # Important:
 # You need to run the following command to generate the ui_facturacion.py file
@@ -14,42 +14,60 @@ from WINDOWS_PY.ui_panelDeControl import Ui_panel_de_control
 class Widget(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.ui_ventana_facturacion = None
+        self.ui_ventana_panel_de_control = None
+        self.ventana_panel_de_control = None
+        self.items = None
         self.ui_login = Ui_Form()
         self.ui_login.setupUi(self)
-
-        #self.ui_login.pushButton.clicked.connect(self.cerrar_ventana)
-        #self.ui_login.pushButton_2.clicked.connect(self.close)
-
-        self.ui_login.pushButton.clicked.connect(self.cerrar_ventana)
-
-        #self.ui_login.pushButton.clicked.connect(self.cerrar_ventana)
-        #self.ui_login.pushButton_2.clicked.connect(self.close)
-        self.ui_login.pushButton.clicked.connect(self.cerrar_ventana)
-
-
-    def cerrar_ventana(self):
-        self.close()
-        #self.facturacion()
-        self.panel_de_control()
+        self.ui_login.pushButton.clicked.connect(self.cerrar_ventana_login)
 
     def panel_de_control(self):
         self.ventana_panel_de_control = QtWidgets.QMainWindow()
         self.ui_ventana_panel_de_control = Ui_panel_de_control()
         self.ui_ventana_panel_de_control.setupUi(self.ventana_panel_de_control)
-        self.ui_ventana_panel_de_control.pushButton_11.clicked.connect(self.facturacion)
+        self.ui_ventana_panel_de_control.pushButton_20.clicked.connect(self.facturacion)
         self.ventana_panel_de_control.show()
-
-
 
     def facturacion(self):
         self.ventana_facturacion = QtWidgets.QMainWindow()
         self.ui_ventana_facturacion = Ui_Widget()
         self.ui_ventana_facturacion.setupUi(self.ventana_facturacion)
+        self.ui_ventana_facturacion.pushButton.clicked.connect(self.agregarItems)
         self.ventana_facturacion.show()
-        self.ui_ventana_facturacion.tableWidget.setColumnWidth(0,182.75)
+
+
+
+    def agregarItems(self):
+        self.items = [
+            {"producto": "Manzana", "cantidad": "5", "precio": "1.000"},
+            {"producto": "Banana", "cantidad": "3", "precio": "500"},
+            {"producto": "Naranja", "cantidad": "2", "precio": "800"},
+        ]
+        # Agregar los ítems a la tabla
+        for item in self.items:
+            row_position = self.ui_ventana_facturacion.tableWidget.rowCount()
+            self.ui_ventana_facturacion.tableWidget.insertRow(row_position)
+
+            self.ui_ventana_facturacion.tableWidget.setItem(row_position, 0, QTableWidgetItem(item["producto"]))
+            self.ui_ventana_facturacion.tableWidget.setItem(row_position, 1, QTableWidgetItem(item["cantidad"]))
+            self.ui_ventana_facturacion.tableWidget.setItem(row_position, 2, QTableWidgetItem(item["precio"]))
+
+
+
+    def cerrar_ventana_login(self):
+        self.close()
+        self.panel_de_control()
+
+
+
+
+
+
+        '''self.ui_ventana_facturacion.tableWidget.setColumnWidth(0,182.75)
         self.ui_ventana_facturacion.tableWidget.setColumnWidth(1,182.75)
         self.ui_ventana_facturacion.tableWidget.setColumnWidth(2,182.75)
-        self.ui_ventana_facturacion.tableWidget.setColumnWidth(3,182.75)
+        self.ui_ventana_facturacion.tableWidget.setColumnWidth(3,182.75)'''
 
 
 if __name__ == "__main__":
