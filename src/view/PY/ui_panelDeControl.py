7d2c171771ffc8f885347bb2e1d3e21c1a741b8d
Gabriel Vicent <rodriguezisaac876@gmail.com>
2025-01-22 18:31:56 -0400

    Nueva estructura de archivos

--- src/view/PY/ui_panelDeControl.py
+++ src/view/PY/ui_panelDeControl.py
@@ -8,16 +8,12 @@
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
-    QMetaObject, QObject, QPoint, QRect,
-    QSize, QTime, QUrl, Qt)
-from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
-    QFont, QFontDatabase, QGradient, QIcon,
-    QImage, QKeySequence, QLinearGradient, QPainter,
-    QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QFrame, QLabel, QPushButton,
-    QSizePolicy, QVBoxLayout, QWidget)
-from QT_UI.iconos_panel_de_control import iconos
+from PySide6.QtCore import (QCoreApplication, QMetaObject, QRect,
+                            QSize, Qt)
+from PySide6.QtGui import (QFont, QIcon)
+from PySide6.QtWidgets import (QFrame, QLabel, QPushButton,
+                               QVBoxLayout, QWidget)
+from src.view.iconos_panel_de_control import iconos
 
 class Ui_panel_de_control(object):
     def setupUi(self, panel_de_control):
